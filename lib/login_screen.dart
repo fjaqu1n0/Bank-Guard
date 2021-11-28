2c580d192ef1fecb75e95e5675b418c1ee47394f
Francis Jacob Aquino <77041731+SleepLezz140@users.noreply.github.com>
2021-11-28 14:40:23 +0000

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -4,126 +4,93 @@ import 'package:flutter/material.dart';
 import 'dashboard.dart';
 
 
-class Login extends StatelessWidget {
-  const Login({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/DashBoard' : (context)=> const DashScreen(),
-      }
-      
-    );
-  }
-}
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController _textController = TextEditingController();
-  TextEditingController _textpassController = TextEditingController();
-  
-  List<String> userCredentials = [
-    'admin',
-  ];
-  List<String> passCredentials = [
-    'admin',
-  ];
-  
-  final formKey = GlobalKey<FormState>(); //key for form
-  String name="";
+   TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    final double height= MediaQuery.of(context).size.height;
-    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
-       body: Container(
-         padding: const EdgeInsets.only(left: 40, right: 40),
-         child: Form(
-            key: formKey, //key for form
-             child:Column(
-               mainAxisAlignment: MainAxisAlignment.start,
-               children: [
-                  SizedBox(height:height*0.05),
-                  Image.asset('assets/images/BankGuard1.png', height: 180,),
-                  SizedBox(height:height*0.05,),
-                  TextFormField(
-                      controller: _textController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(40)),
-                         hintText: 'Username',
-                        hintStyle: TextStyle(color: Colors.black),
-                      labelText: "Username",
-                      fillColor: Colors.white,
-                      filled: true,
-                    ),
-                  ),
-                  SizedBox(height:height*0.05,),
-                  TextFormField(
-                     controller: _textpassController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(40))
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Colors.black),
-                      labelText: "Password",
-                      fillColor: Colors.white,
-                      filled: true,
-                    ),
-                  ),
-                  SizedBox(height:height*0.05, width: 200,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: (){
-                         if (!userCredentials.contains(_textController.text.toLowerCase()) ||(!passCredentials.contains(_textpassController.text.toLowerCase()))) 
-                          {
-                            setState(() 
-                            {
-                              final snackBar = SnackBar(
-                                  content: Text('Incorrect Username/Password'));
-                                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                            });
-                          } else 
-                          {
-                            setState(() 
-                            {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const DashBoard()),
-                              );
-                            });
-                          }
-                        },
-                        child: Text('Login'),
-                        style: ElevatedButton.styleFrom(
-                        minimumSize: Size(110, 50),
-                        textStyle: TextStyle(fontSize: 20),
-                        primary: Color(0xffffac30),
-                        onPrimary: Colors.white,)
-                      ),
-                      ],
-                  )
-                ],
-             )
-         )
-       )
-    );
+        body: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/BankGuard1.png',),
+            SizedBox(width: 5,),
+            SizedBox(height: 5,),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(40)),
+                hintText: 'Username',
+                hintStyle: TextStyle(color: Colors.black),
+                fillColor: Color(0xFFF3E5F5),
+                filled: true,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(40)),
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.black),
+                fillColor: Color(0xFFF3E5F5),
+                filled: true,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                child: const Text('Login',style: TextStyle(fontSize: 15),),
+                style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all(Color(0xffffac30)),
+                    elevation: MaterialStateProperty.all(0),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)))),
+                onPressed: () 
+                {
+                authentication(usernameController.text, passwordController.text, context);
+                },    
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
-  void openDashBoard()
-  {
-    Navigator.pushNamed(context, '/DashBoard');
+  void authentication(String username, String password, BuildContext context) {
+    if (username == 'admin' && password == 'admin') {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const DashBoard()));
+    } else {
+      SnackBar snackBar = SnackBar(
+          content: const Text('Invalid Credentials.'),
+          action: SnackBarAction(label: 'Close', onPressed: () {}));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 }
