56f2e778b6206c1baf1c55ca584881208499aef8
George Cabanlit III <75512689+R3cluse@users.noreply.github.com>
2021-11-28 22:46:06 +0800

    Delete lib/Utility directory

--- lib/Utility/custom_functions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-String formatCurrency(num amount,{int decimalCount = 0}){
-  final formatCurrency = new NumberFormat.simpleCurrency(decimalDigits: decimalCount);
-  return formatCurrency.format(amount);
-}
