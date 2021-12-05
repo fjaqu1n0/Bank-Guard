8935c309b09cad9151ede77571e11b82fbed8247
Francis Jacob Aquino <77041731+SleepLezz140@users.noreply.github.com>
2021-12-05 14:02:35 +0000

    Add files via upload

---
+++ lib/authentication.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'login_screen.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 9),
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context)
+                    {
+                      return LoginScreen();
+                    },
+                  )
+                  );
+                },
+                child: Text('Login'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(210, 50),
+                  textStyle: TextStyle(fontSize: 20),
+                  primary: Color(0xffffac30),
+                  onPrimary: Colors.black,
+                ),
+              )
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 9),
+              child: ElevatedButton(
+                onPressed: ()=> exit(0),
+                child: Text('Exit'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(210, 50),
+                  textStyle: TextStyle(fontSize: 20),
+                  primary: Color(0xffffac30),
+                  onPrimary: Colors.black,
+                ),
+              )
+            )
+          ],
+        ))
+      
+    );
+  }
+}
