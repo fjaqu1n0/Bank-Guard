6e14edae4845240781ee0d7af5e84f3465c4bfc8
Francis Jacob Aquino <77041731+SleepLezz140@users.noreply.github.com>
2021-12-06 14:16:57 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'authentication.dart';
-import 'package:money_transfer_app/providers/balance.dart';
+import 'package:sdev_app/providers/balance.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
