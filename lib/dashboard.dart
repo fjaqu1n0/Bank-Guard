70e13afc212a43478ee8699ee8babe4cca103489
Francis Jacob Aquino <77041731+SleepLezz140@users.noreply.github.com>
2021-12-06 10:04:36 +0000

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:money_transfer_app/billing_screen.dart';
 import 'package:provider/provider.dart';
 import 'send_money.dart';
 import 'package:money_transfer_app/providers/balance.dart';
-import 'service_money.dart';
 
 class DashBoard extends StatelessWidget {
   const DashBoard({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:  DashScreen(),
-    );
-  }
-}
-
-class DashScreen extends StatefulWidget {
-  const  DashScreen({ Key? key }) : super(key: key);
-
-  @override
-  _DashScreenState createState() => _DashScreenState();
-}
-
-class _DashScreenState extends State< DashScreen> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
+      
       backgroundColor: const Color(0xfff5f5f5),
       body: Container(
         padding: const EdgeInsets.all(30),
@@ -95,6 +79,7 @@ class _DashScreenState extends State< DashScreen> {
             SizedBox( height: 10,),
             SizedBox(height: 20,),
             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text('Send Money', style: TextStyle(
                   fontSize: 21,
@@ -127,18 +112,210 @@ class _DashScreenState extends State< DashScreen> {
               child: GridView.count(crossAxisCount: 4,
               childAspectRatio: 0.7,
               children: [
-                serviceWidget("school", "School"),
-                serviceWidget("health", "Healthcare"),
-                serviceWidget("electricity", "Electricity\nBill"),
-                serviceWidget("movie", "Movie\nTicket"),
-                serviceWidget("flight", "Flight\nTicket"),
-                serviceWidget("water", "Water\nBill"),
-                serviceWidget("telco", "Telecom\nServices"),
-                serviceWidget("government", "Government"),
-                serviceWidget("internet", "Cable/Internet"),
-                serviceWidget("transpo", "Transportation"),
-                serviceWidget("credit", "Credit/Debit"),
-                serviceWidget("insurance", "Insurance"),
+                 Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(0, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 9, name: 'School'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(1, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 5, name: 'Healthcare'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(2, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 2, name: 'Electric Utilities'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(3, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 8, name: 'Cinema'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(4, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 3, name: 'Airlines'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(5, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 12, name: 'Water Utilities'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(6, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 10, name: 'Telecommunications'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(7, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 4, name: 'Government'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(8, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 7, name: 'Cable/Internet'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(9, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 11, name: 'Land Transportation'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(10, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 1, name: 'Credit/Debit'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Color(0xfff1f3f6),
+                    child: InkWell(
+                      onTap: () {
+                        setValue(11, context);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ServiceIcon(id: 6, name: 'Insurance'),
+                        ],
+                      ),
+                    ),
+                  ),
               ],
               ),
             )
@@ -148,42 +325,37 @@ class _DashScreenState extends State< DashScreen> {
       );
     
   }
-  Column serviceWidget(String img, String name)
-  {
+  
+}
+class ServiceIcon extends StatelessWidget {
+  final String name;
+  final int id;
+  const ServiceIcon({Key? key, required this.id, required this.name})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          child: InkWell(
-            onTap: ()
-            {
-              Navigator.push(context,MaterialPageRoute(builder: (context) => const ServiceMoney()));
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Color(0xfff1f3f6)
-              ),
-              child: Center(
-                child: Container(
-                  margin: EdgeInsets.all(25),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/$img.png') 
-                      )
-                  ),
-                ),
-              ),
-            ),
-            ),
-          ),
-          SizedBox(height: 5,),
-          Text(name, style: TextStyle(
+          CircleAvatar(
+          radius: 25,
+          backgroundColor: Color(0xfff1f3f6),
+          backgroundImage: AssetImage('assets/images/service$id.png'),
+          ),  
+        SizedBox(height: 10,),
+        Text('$name',style: TextStyle(
             fontFamily: 'avenir',
             fontSize: 14,
           ),textAlign: TextAlign.center,)
       ],
     );
-
   }
-
 }
+
+void setValue(int service, BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => Merchants(service)),
+  );
+}
+
